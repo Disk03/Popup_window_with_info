688cdd0e237019f821e7f9224e81578e69d530c0
Dmitry <korobko.dmitry@ratkussoft.com>
2017-09-03 02:09:57 +0300

    Edit styles

--- scripts.js
+++ scripts.js
@@ -3,17 +3,22 @@ $(document).ready(function () {
     setTimeout(function(){
         $.getJSON('http://dev.nexusmedia-ua.com/devtest/', function (response) {
             var dataFromJson = JSON.parse(JSON.stringify(response.messages[0], null, 4)),
-                dataImage = $('<p style="text-align: center">Image:</p><div style="text-align: center;"><img src="' + dataFromJson.image + '" style="width: 100px; height: 100px"></div>'),
-                dataName = $('<p style="text-align: center">Name:</p><h5 style="text-align: center;">' + dataFromJson.name + '</h5>'),
-                dataTitle = $('<p style="text-align: center">Title:</p><h5 style="text-align: center;">' + dataFromJson.title + '</h5>'),
-                dataMessage = $('<p style="text-align: center">Message:</p><h5 style="text-align: center;">' + dataFromJson.message + '</h5>'),
+                dataImage = $('<div style="display:inline-block"><img src="' + dataFromJson.image + '" style=" border-radius: 50px; width: 50px; height: 50px"></div>'),
+                dataName = $('<h5 style="text-align: left;">' + dataFromJson.name + '</h5>'),
+                dataTitle = $('<span style="text-align: left;">' + dataFromJson.title + '</span>'),
+                dataNameTitle = $('<div style="display:inline-block; margin-left: 5%"></div>'),
+                dataImageNameTitle = $('<div style="margin-left: 5%; margin-top: 5%"></div>'),
+                dataMessage = $('<h4 style="text-align: left; margin-left: 5%">' + dataFromJson.message + '</h4>'),
                 body = $("body"),
-                divMain = $('<div id="main_div" style=" position: fixed; bottom: 5%; right: 5%; z-index: 999999; border-radius: 25px; background-color: lightsteelblue; width: 300px; height: 500px"></div>'),
-                closeOpenButton = $('<div><img id="close_open_button" src="http://images.all-free-download.com/images/graphiclarge/round_red_x_sign_4229.jpg" alt="Close/Open button" style="border-radius: 25px; position: fixed; bottom: 3%; right: 3%; width: 25px"></div>');
+                divMain = $('<div id="main_div" style=" position: fixed; bottom: 5%; right: 5%; z-index: 1000; border-radius: 25px; background-color: lightsteelblue; width: 300px; height: 250px"></div>'),
+                closeOpenButton = $('<div><img id="close_open_button" src="http://images.all-free-download.com/images/graphiclarge/round_red_x_sign_4229.jpg" alt="Close/Open button" style=" z-index: 2000;border-radius: 25px; position: fixed; bottom: 4%; right: 4%; width: 25px"></div>');
 
-            divMain.append(dataTitle);
-            divMain.append(dataImage);
-            divMain.append(dataName);
+
+            dataNameTitle.append(dataName);
+            dataNameTitle.append(dataTitle);
+            dataImageNameTitle.append(dataImage);
+            dataImageNameTitle.append(dataNameTitle);
+            divMain.append(dataImageNameTitle);
             divMain.append(dataMessage);
             body.append(closeOpenButton);
             body.append(divMain);
